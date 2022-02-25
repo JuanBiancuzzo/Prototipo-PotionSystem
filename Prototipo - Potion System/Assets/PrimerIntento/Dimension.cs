2529f9ef8aac22a771bb1d31d1e4becc68a38c90
Juan Biancuzzo <jbiancuzzo@fi.uba.ar>
2022-02-25 16:42:07 -0300

    Eliminando el primer intento, no es necesario ya

--- Prototipo - Potion System/Assets/PrimerIntento/Dimension.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-
-namespace ItIsNotOnlyMe
-{
-    [CreateAssetMenu(fileName = "Dimension", menuName = "Espacio/Dimension")]
-    public class Dimension : ScriptableObject
-    {
-    }
-}
